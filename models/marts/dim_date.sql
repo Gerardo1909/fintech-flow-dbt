f68fcf818259366f5859fb9704e9f85a2488caeb
Gerardo1909 <gerardotoboso1909@gmail.com>
2026-01-28 21:36:25 -0300

    Arregla error en generacion de fechas en tabla de dimension de fechas.

--- models/marts/dim_date.sql
+++ models/marts/dim_date.sql
@@ -1,8 +1,26 @@
-select
-    datum as date_day,
-    extract(year from datum) as year,
-    extract(month from datum) as month,
-    to_char(datum, 'TMMonth') as month_name,
-    extract(dow from datum) in (0, 6) as is_weekend
+{{
+    config(
+        materialized='external',
+        format='csv',
+        location="data/output/dim_date.csv",
+        options={
+            "overwrite": True
+        }
+    )
+}}
 
-from generate_series('2023-01-01'::date, '2036-12-31'::date, '1 day'::interval) as datum
+WITH date_spine AS (
+    {{ dbt_utils.date_spine(
+        datepart="day",
+        start_date="cast('2023-01-01' as date)",
+        end_date="cast('2026-12-31' as date)"
+    ) }}
+)
+
+select
+    date_day,
+    date_part('year', date_day) as year,
+    date_part('month', date_day) as month,
+    date_part('dow', date_day) in (0, 6) as is_weekend
+
+from date_spine
